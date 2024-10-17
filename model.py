c33fa77fb5f1bb8b549a50d4abf0c8625b6081aa
Camil Ziane <camil.ziane@epita.fr>
2024-10-17 13:57:47 +0200

    feat: load not builded model

--- model.py
+++ model.py
@@ -1,22 +1,30 @@
+data_path = "data/houses.csv"
+model_path = "models/regression.joblib"
+
+
 def build_model():
     import pandas as pd
     from sklearn.linear_model import LinearRegression
     import joblib
     import os
 
-    df = pd.read_csv("data/houses.csv")
+    df = pd.read_csv(data_path)
     X = df[["size", "nb_rooms", "garden"]]
     y = df["price"]
     model = LinearRegression()
     model.fit(X, y)
     os.makedirs("models", exist_ok=True)
-    joblib.dump(model, "models/regression.joblib")
+    joblib.dump(model, model_path)
 
 
 def load_model():
     import joblib
+    import os
 
-    model = joblib.load("models/regression.joblib")
+    if not os.path.exists(model_path):
+        build_model()
+
+    model = joblib.load(model_path)
     return model
 
 
